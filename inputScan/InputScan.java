b26acd2e4a5841852eb53eb7e351fc7cdd35e873
VinuBalagopalAP <vinubalagopalap@gmail.com>
2022-02-01 20:04:53 +0530

    input & scanning

---
+++ inputScan/InputScan.java
@@ -0,0 +1,18 @@
+package inputScan;
+
+import java.util.Scanner;
+
+public class InputScan {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = scan.nextInt();
+        System.out.println("Enter a name: ");
+        String name = scan.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Hello " + name + "!");
+        }
+    }
+
+}
